c2ad0da5e809cf8478b4023a209b54061f543017
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2026-01-12 10:39:16 +0530

    Update LeadersOfPeople.jsx

--- frontend/src/pages/LeadersOfPeople.jsx
+++ frontend/src/pages/LeadersOfPeople.jsx
@@ -10,8 +10,8 @@ const LeadersOfPeople = () => {
   return (
     <div className="min-h-screen bg-white">
       <Navbar/>
-      <BreadCrumb title={'About Us'} bgImage={'/images/breadcrum/about.png'}/>
-      {/* Leadership Team Section */}
+      {/* <BreadCrumb title={'Leaders & People'} bgImage={'/images/breadcrum/about.png'}/>
+       Leadership Team Section */}
       <section className="bg-gradient-to-br from-gray-50 to-green-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <LeadershipTeam />
